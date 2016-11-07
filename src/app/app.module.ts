5ade92ae66a8f81a2fd3334bd91027cd94643beb
Jeffrey Yoo <jeffyoo216@gmail.com>
2016-11-07 14:04:17 -0800

    [setup] made changes to the file structure to follow john papa's style guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './shared/login.component';
+import { SignoutComponent } from './shared/signout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    MainComponent,
+    LoginComponent,
+    SignoutComponent
   ],
   imports: [
     BrowserModule,
