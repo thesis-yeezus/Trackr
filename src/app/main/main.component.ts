9d33a8211e5e97fcfa96588b3fea1d7ed2c6a510
Michael Yang <michaelyang776@gmail.com>
2016-11-08 11:57:27 -0800

    [setup] Set up basic ng-grid

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GridOptions, IFilter } from 'ag-grid/main';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  private gridOptions: GridOptions;
+  private showGrid: boolean;
+  private rowData: any[];
+  private columnDefs: any[];
+  private rowCount: string;
+
+  constructor() {
+    // we pass an empty gridOptions in, so we can grab the api out
+    this.gridOptions = <GridOptions>{};
+    this.createRowData();
+    this.showGrid = true;
+   }
 
   ngOnInit() {
+    
+  }
+
+  private createRowData() {
+    var rowData: any[] = [];
   }
 
 }
