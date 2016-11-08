de9a42c8ac91f5b33783ff380e2dabe59e27f454
Jeffrey Yoo <jeffyoo216@gmail.com>
2016-11-08 11:08:24 -0800

    [feature] made forms and modals for signup page

--- src/app/shared/login.component.ts
+++ src/app/shared/login.component.ts
@@ -3,28 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   template: `
-    <p>
-      login Works!
-    </p>
-<button class="ui button green" (click)="myModal.show()">Click me</button>
-<sm-modal title="Hello from Modal" class="basic" #myModal>
-    <modal-content>
-        ...
-    </modal-content>
-</sm-modal>
-
+    <button class="right attached ui button" (click)="loginModal.show({inverted:false})">Login</button>
+    <sm-modal title="Login" class="basic" #loginModal>
+        <modal-content>
+          <form class="ui form" (click)="test()">
+            <div class="field">
+              <label id="inputLabel">Username</label>
+              <input type="text" placeholder="Username">
+            </div>
+            <div class="field">
+              <label id="inputLabel">Password</label>
+              <input type="password" placeholder="Password">
+            </div>
+            <button class="ui button" type="submit">Submit</button>
+          </form>
+        </modal-content>
+    </sm-modal>
   `,
-  styles: []
+  styles: [`
+   #inputLabel {
+     color: white;
+   } 
+  `]
 })
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  test(){
-    alert('sup');
   }
 
 }
