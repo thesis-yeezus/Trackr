49398a101ae57cd1ffee268aaee18877496fa40e
Jeffrey Yoo <jeffyoo216@gmail.com>
2016-11-07 23:49:48 -0800

    [bug] trying to fix sm-modal

--- src/app/shared/login.component.ts
+++ src/app/shared/login.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
     <p>
       login Works!
     </p>
+<button class="ui button green" (click)="myModal.show()">Click me</button>
+<sm-modal title="Hello from Modal" class="basic" #myModal>
+    <modal-content>
+        ...
+    </modal-content>
+</sm-modal>
+
   `,
   styles: []
 })
@@ -14,6 +21,10 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  test(){
+    alert('sup');
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../shared/login.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+    // let loginService = new LoginService;
+  }
+  // test(){
+  //   alert('sup')
+  // }
+  test(){
+    this.loginService.modal();
   }
 
 }
