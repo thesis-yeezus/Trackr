fe7f27504e527637c0053ad0f17d2e204dc9ad84
Jeffrey Yoo <jeffyoo216@gmail.com>
2016-11-09 08:03:31 -0800

    [refactor] redid the db, server/model, and controllers. Follow this template for future reference. still need to sort out db.js and index.js in db

---
+++ server/db/coachSeeker.js
@@ -0,0 +1,9 @@
+var Sequelize = require('sequelize');
+
+module.exports = function(db) {
+  var Coach_Seeker = db.define('coachSeeker', {
+    name: { type: Sequelize.STRING }
+  });
+
+  return Coach_Seeker;
+};
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,15 @@
 // var Blog = require('./blog');
 // var JobOpening = require('./jobOpening');
 // var Keyword = require('./keyword');
-// var User = require('./user');
+var userModel = require('./user.model');
 // var Coach_Seeker = require('./coachSeeker');
 // var SharedJob = require('./sharedJob');
 
-// module.exports = {
+module.exports = {
 //   Blog: Blog,
 //   JobOpening: JobOpening,
 //   Keyword: Keyword,
-//   User: User,
+  userModel: userModel,
 //   Coach_Seeker: Coach_Seeker,
 //   SharedJob: SharedJob
-// };
+};
