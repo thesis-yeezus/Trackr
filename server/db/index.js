bedd5fddfe2c77eb28262c750a40978f17c9c2dd
oliv3rwang <oliv3r.wang@gmail.com>
2016-11-18 09:20:24 -0800

    [feature] Cron jobs for daily/weekly/threeday interval email reminders complete

--- server/db/index.js
+++ server/db/index.js
@@ -6,6 +6,7 @@ var Blog = require('./blog')(db);
 var Keyword = require('./keyword')(db);
 var Coach_Seeker = require('./coachSeeker')(db);
 var SharedJob = require('./sharedJob')(db);
+var Setting = require('./keyword')(db);
 
 // User.belongsToMany(User, {as: 'Coach', through: 'coachSeeker', foreignKey: 'coach_id'})
 // User.belongsToMany(User, {as: 'Seeker', through: 'coachSeeker', foreignKey: 'seeker_id'})
@@ -14,14 +15,13 @@ User.belongsToMany(User, {as: 'Coach_Seeker', through: 'coachSeeker', foreignKey
 // User.belongsToMany(User, {as: 'coach_id', through: 'coachSeeker'});
 User.hasOne(Blog);
 User.hasOne(JobOpening);
+User.hasOne(Setting);
 Blog.belongsToMany(Keyword, {through: 'BlogKeyword'});
 Keyword.belongsToMany(Blog, {through: 'BlogKeyword'});
 // JobOpening.belongsToMany(Keyword, {through: 'JobOpeningKeyword'});
 // Keyword.belongsToMany(JobOpening, {through: 'JobOpeningKeyword'});
 
 // JobOpening.belongsToMany(JobOpening, {as: 'JobPosting', through: 'SharedJobs', foreignKey: 'jobOpening_id'} )
-
-// ASK MARCO ABOUT THIS SELF-RELATIONAL TABLE.
 
 // SharedJob.hasOne(User, {as: 'Giver'});
 // SharedJob.hasMany(User, {as: 'Receiver'});
@@ -37,5 +37,6 @@ module.exports = {
 	Blog: Blog,
 	Keyword: Keyword,
 	Coach_Seeker: Coach_Seeker,
-	SharedJob: SharedJob
+	SharedJob: SharedJob,
+	Setting: Setting
 };
