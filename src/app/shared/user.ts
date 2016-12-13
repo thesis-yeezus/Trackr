606a93066484cfe5c55ad5e9ff914eb593ac56e3
Michael Yang <micyang@users.noreply.github.com>
2016-12-12 16:19:39 -0800

    Update user.ts

--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -1,16 +1,7 @@
-// export class User {
-//     username: string;
-//     password: string;
-//     firstName: string;
-//     lastName: string;
-// }
-
 export class User {
-  // id: number;
   firstName: string;
   lastName: string;
   username: string;
   password: string;
   email: string;
-  // role: string;
 }
