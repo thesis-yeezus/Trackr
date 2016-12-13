3b6c7133adab24c787f0aa2242ff53eaf427725e
Michael Yang <micyang@users.noreply.github.com>
2016-12-12 16:22:24 -0800

    Update signup-settings.component.ts

--- src/app/signup-settings/signup-settings.component.ts
+++ src/app/signup-settings/signup-settings.component.ts
@@ -30,16 +30,7 @@ export class SignupSettingsComponent implements OnInit {
     private router: Router,
     private fb: FormBuilder
     ) {
-      // this.signupForm = this.fb.group({
-      //   'firstName': [null, Validators.required], 
-      //   'lastName': [null, Validators.required], 
-      //   'username': [null, Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(15)])], //min, max, unique
-      //   'password': [null, Validators.compose([Validators.required, Validators.pattern("^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$")])],
-      //   'confirmPassword': [null, Validators.compose([Validators.required, Validators.pattern("^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$")])], 
-      //   'email': [null, Validators.compose([Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")])] //must be an email address
-      // })
       this.signupSettingsForm = this.fb.group({
-        // 'goals': "",
         'setGoals': null, //a number
         'receiveEmails': null // true or false
       })
@@ -50,7 +41,7 @@ export class SignupSettingsComponent implements OnInit {
   applySignupSettings(settings: any) {
     console.log('This is settings:', settings);
     var userId: any = window.localStorage["userId"];
-    //set information to localStorage
+    // set information to localStorage
     // After sending this information to SAVE, redirect to the Main Page
     this.userService.applySignupSettings(settings, userId)
     var here = this;
